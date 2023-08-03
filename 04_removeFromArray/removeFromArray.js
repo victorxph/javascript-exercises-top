7938b8f994809ef95d32fe5acb8a2cdf0fd747a8
victor_rxph <v.rxphdev@gmail.com>
2023-08-03 19:37:49 -0300

    Finish 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const removeFromArray = function(array) {
+
+    for (i = 1; i <= arguments.length; i++){
+
+        let toBeRemovedIndex = arguments[i];
+
+        let index = array.indexOf(toBeRemovedIndex);
+
+        if (index !== -1){
+
+            array.splice(index, 1)
+
+        }
+        
+    }
+    
+    // console.log(array)
+    return array
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
