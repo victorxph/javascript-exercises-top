46b90159ae17bc309b7883100b75e044936ed36d
victor_rxph <v.rxphdev@gmail.com>
2023-08-03 16:12:40 -0300

    Initial commit
    Finish 01, 02 and 03.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let res = ''
+
+    if (num < 0) {
+
+        return 'ERROR'
+
+    }
+
+    for (i = 1; i <= num; i++) {
+
+        res += string
+        
+    }
+    
+    // console.log(res)
+    return res
 
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,35 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    // index = string.length
+    
+    let reversedString = '';
+
+    for (i = string.length; i > 0; i--){
+
+        let char = string.substr(i - 1, 1)
+
+        reversedString += char;
+
+
+        
+    }
+
+    console.log(reversedString);
+    return reversedString;
 
 };
 
+
+
+// Algo
+/*
+
+Declarar index = string.length
+Pegar o ultimo char com substr(index - 1, 1)
+Armazenar let = char
+A cada iteração index--
+res += char enquanto index > 0
+
+*/
 // Do not edit below this line
 module.exports = reverseString;
