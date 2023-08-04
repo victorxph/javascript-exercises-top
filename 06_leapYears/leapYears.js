21f3be335f309bb429b4da831ec1dd9bad18b2af
victor_rxph <v.rxphdev@gmail.com>
2023-08-04 17:19:25 -0300

    Finish 06 and 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,37 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 400 === 0) {
+        
+        console.log('case 1:', year % 400 === 0);
+        return true
+
+    } else if (year % 100 === 0 && !year % 400 === 0) {
+        
+        console.log('case 2:', year % 100 && !year % 400 === 0);
+        return false
+
+    } else if (year % 4 === 0) {
+
+        console.log('case 3:', year % 4 === 0);
+        return true
+
+    } else {
+
+        return false
+
+    }
 };
+
+console.log(leapYears(1996));
+
+/*
+Must be:
+Divisivel por 4
+
+
+not:
+Divisivel por 100 a menos que seja divisivel por 400
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+
+  let celsius = (num - 32) * 5 / 9
+
+  console.log(celsius.toFixed(1));
+  
+  return parseFloat(celsius.toFixed(1))
+  
 };
 
-const convertToFahrenheit = function() {
+// convertToCelsius(75);
+
+const convertToFahrenheit = function(num) {
+  
+  let fahrenheit = num * 9 / 5 + 32 
+  
+  console.log(fahrenheit.toFixed(1));
+  
+  return parseFloat(fahrenheit.toFixed(1))
+
 };
+
+// convertToFahrenheit(23.9);
+
+// To celsius : x °F ≘ (x − 32) × 5/9 °C
 
 // Do not edit below this line
 module.exports = {
